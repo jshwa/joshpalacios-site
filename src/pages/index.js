47fa6dba0fcf0a3562f48d2563fc535337b9d026
jshwa <32717724+jshwa@users.noreply.github.com>
2018-04-10 15:06:02 -0500

    basic styling for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
+import styles from '../css/index.module.css';
+import '../css/index.css';
 
-const IndexPage = (props) => (
-  <div>
+const IndexPage = props => (
+  <div class={styles.wrapper}>
+    <div class={styles.img}>
      <Img sizes={props.data.josh.sizes} />
+    </div>
   </div>
 )
 
