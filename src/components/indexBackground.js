d19274a722e11549762842b330eb69d8c3318680
jshwa <32717724+jshwa@users.noreply.github.com>
2018-04-18 00:37:05 -0500

    dynamically change preserveAspectRatio based on screen width for best size of svg, add extra rows of text to fill in background for tall screens

--- src/components/indexBackground.js
+++ src/components/indexBackground.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 export const DebateBackground = props => {
    return (
-      <svg width="70vw" height="100vh" viewBox="0 0 500 800" preserveAspectRatio="xMinYMid slice">
+      <svg width="60vw" height="100vh" viewBox="0 0 500 800" preserveAspectRatio={`xMinYMid ${props.PARval}`}>
+         <text x="0" y="-300" class={`${props.background} ${props.hide}`}>DEBATECOACHDEBATE
+            <animate id="scrollanim39" attributeName="x" from="-75" to="0" dur="9s" begin="0s; scrollanim38.end" />
+            <animate id="scrollanim38" attributeName="x" from="0" to="-75" dur="18s" begin="scrollanim39.end" />
+         </text>
+         <text x="0" y="-230" class={`${props.background} ${props.hide}`}>ECOACHDEBATECOACH
+            <animate id="scrollanim37" attributeName="x" from="0" to="-100" dur="7s" begin="0s; scrollanim36.end" />
+            <animate id="scrollanim36" attributeName="x" from="-100" to="0" dur="10s" begin="scrollanim37.end" />
+         </text>
+         <text x="0" y="-160" class={`${props.background} ${props.hide}`}>OACHDEBATECOACHDE
+            <animate id="scrollanim35" attributeName="x" from="-50" to="0" dur="7s" begin="0s; scrollanim34.end" />
+            <animate id="scrollanim34" attributeName="x" from="0" to="-50" dur="14s" begin="scrollanim35.end" />
+         </text>
+         <text x="0" y="-90" class={`${props.background} ${props.hide}`}>DEBATECOACHDEBATE
+            <animate id="scrollanim33" attributeName="x" from="-75" to="0" dur="9s" begin="0s; scrollanim32.end" />
+            <animate id="scrollanim32" attributeName="x" from="0" to="-75" dur="18s" begin="scrollanim33.end" />
+         </text>
+         <text x="0" y="-20" class={`${props.background} ${props.hide}`}>ECOACHDEBATECOACH
+            <animate id="scrollanim30" attributeName="x" from="0" to="-100" dur="7s" begin="0s; scrollanim31.end" />
+            <animate id="scrollanim31" attributeName="x" from="-100" to="0" dur="10s" begin="scrollanim30.end" />
+         </text>
          <text x="0" y="50" class={`${props.background} ${props.hide}`}>OACHDEBATECOACHDE
             <animate id="scrollanim1" attributeName="x" from="-50" to="0" dur="7s" begin="0s; scrollanim2.end" />
             <animate id="scrollanim2" attributeName="x" from="0" to="-50" dur="14s" begin="scrollanim1.end" />
@@ -47,56 +67,116 @@ export const DebateBackground = props => {
             <animate id="scrollanim17" attributeName="x" from="0" to="-100" dur="7s" begin="0s; scrollanim18.end" />
             <animate id="scrollanim18" attributeName="x" from="-100" to="0" dur="10s" begin="scrollanim17.end" />
          </text>
+         <text x="0" y="890" class={`${props.background} ${props.hide}`}>OACHDEBATECOACHDE
+            <animate id="scrollanim21" attributeName="x" from="-50" to="0" dur="7s" begin="0s; scrollanim22.end" />
+            <animate id="scrollanim22" attributeName="x" from="0" to="-50" dur="14s" begin="scrollanim21.end" />
+         </text>
+         <text x="0" y="960" class={`${props.background} ${props.hide}`}>DEBATECOACHDEBATE
+            <animate id="scrollanim23" attributeName="x" from="-25" to="-50" dur="10s" begin="0s; scrollanim24.end" />
+            <animate id="scrollanim24" attributeName="x" from="-50" to="-25" dur="12s" begin="scrollanim23.end" />
+         </text>
+         <text x="0" y="1030" class={`${props.background} ${props.hide}`}>DEBATECOACHDEBATE
+            <animate id="scrollanim25" attributeName="x" from="-75" to="0" dur="9s" begin="0s; scrollanim26.end" />
+            <animate id="scrollanim26" attributeName="x" from="0" to="-75" dur="18s" begin="scrollanim25.end" />
+         </text>
+         <text x="0" y="1100" class={`${props.background} ${props.hide}`}>ECOACHDEBATECOACH
+            <animate id="scrollanim27" attributeName="x" from="0" to="-100" dur="7s" begin="0s; scrollanim28.end" />
+            <animate id="scrollanim28" attributeName="x" from="-100" to="0" dur="10s" begin="scrollanim27.end" />
+         </text>
+         <text x="0" y="1170" class={`${props.background} ${props.hide}`}>OACHDEBATECOACHDE
+            <animate id="scrollanim29" attributeName="x" from="-50" to="0" dur="7s" begin="0s; scrollanim30.end" />
+            <animate id="scrollanim30" attributeName="x" from="0" to="-50" dur="14s" begin="scrollanim29.end" />
+         </text>
       </svg>
    )
 }
 
 export const DevBackground = props => {
    return (
-      <svg width="70vw" height="100vh" viewBox="-500 0 500 800" preserveAspectRatio="xMaxYMid slice">
+      <svg width="60vw" height="100%" viewBox="-500 0 500 800" preserveAspectRatio={`xMaxYMid ${props.PARval}`}>
+         <text x="0" y="-300" text-anchor="end" class={`${props.background} ${props.hide}`}>WEBDEVELOPERWEBDE
+            <animate id="devscrollanim31" attributeName="x" from="0" to="100" dur="7s" begin="0s; devscrollanim32.end" />
+            <animate id="devscrollanim32" attributeName="x" from="100" to="0" dur="10s" begin="devscrollanim31.end" />
+         </text>
+         <text x="0" y="-230" text-anchor="end" class={`${props.background} ${props.hide}`}>EVELOPERWEBDEVELO
+            <animate id="devscrollanim33" attributeName="x" from="75" to="0" dur="9s" begin="0s; devscrollanim34.end" />
+            <animate id="devscrollanim34" attributeName="x" from="0" to="75" dur="18s" begin="devscrollanim33.end" />
+         </text>
+         <text x="0" y="-160" text-anchor="end" class={`${props.background} ${props.hide}`}>PERWEBDEVELOPERWE
+            <animate id="devscrollanim35" attributeName="x" from="25" to="50" dur="7s" begin="0s; devscrollanim36.end" />
+            <animate id="devscrollanim36" attributeName="x" from="50" to="25" dur="15s" begin="devscrollanim35.end" />
+         </text>
+         <text x="0" y="-90" text-anchor="end" class={`${props.background} ${props.hide}`}>LOPERWEBDEVELOPER
+            <animate id="devscrollanim9" attributeName="x" from="50" to="0" dur="12s" begin="0s; devscrollanim10.end" />
+            <animate id="devscrollanim10" attributeName="x" from="0" to="50" dur="19s" begin="devscrollanim9.end" />
+         </text>
+         <text x="0" y="-20" text-anchor="end" class={`${props.background} ${props.hide}`}>EBDEVELOPERWEBDEV
+            <animate id="devscrollanim19" attributeName="x" from="100" to="0" dur="15s" begin="0s; devscrollanim20.end" />
+            <animate id="devscrollanim20" attributeName="x" from="0" to="100" dur="15s" begin="devscrollanim19.end" />
+         </text>
          <text x="0" y="50" text-anchor="end" class={`${props.background} ${props.hide}`}>WEBDEVELOPERWEBDE
-            <animate id="scrollanim17" attributeName="x" from="0" to="100" dur="7s" begin="0s; scrollanim18.end" />
-            <animate id="scrollanim18" attributeName="x" from="100" to="0" dur="10s" begin="scrollanim17.end" />
+            <animate id="devscrollanim17" attributeName="x" from="0" to="100" dur="7s" begin="0s; devscrollanim18.end" />
+            <animate id="devscrollanim18" attributeName="x" from="100" to="0" dur="10s" begin="devscrollanim17.end" />
          </text>
          <text x="0" y="120" text-anchor="end" class={`${props.background} ${props.hide}`}>EVELOPERWEBDEVELO
-            <animate id="scrollanim15" attributeName="x" from="75" to="0" dur="9s" begin="0s; scrollanim16.end" />
-            <animate id="scrollanim16" attributeName="x" from="0" to="75" dur="18s" begin="scrollanim15.end" />
+            <animate id="devscrollanim15" attributeName="x" from="75" to="0" dur="9s" begin="0s; devscrollanim16.end" />
+            <animate id="devscrollanim16" attributeName="x" from="0" to="75" dur="18s" begin="devscrollanim15.end" />
          </text>
          <text x="0" y="190" text-anchor="end" class={`${props.background} ${props.hide}`}>PERWEBDEVELOPERWE
-            <animate id="scrollanim13" attributeName="x" from="25" to="50" dur="7s" begin="0s; scrollanim14.end" />
-            <animate id="scrollanim14" attributeName="x" from="50" to="25" dur="15s" begin="scrollanim13.end" />
+            <animate id="devscrollanim13" attributeName="x" from="25" to="50" dur="7s" begin="0s; devscrollanim14.end" />
+            <animate id="devscrollanim14" attributeName="x" from="50" to="25" dur="15s" begin="devscrollanim13.end" />
          </text>
          <text x="0" y="260" text-anchor="end" class={`${props.background} ${props.hide}`}>LOPERWEBDEVELOPER
-            <animate id="scrollanim9" attributeName="x" from="50" to="0" dur="12s" begin="0s; scrollanim10.end" />
-            <animate id="scrollanim10" attributeName="x" from="0" to="50" dur="19s" begin="scrollanim9.end" />
+            <animate id="devscrollanim9" attributeName="x" from="50" to="0" dur="12s" begin="0s; devscrollanim10.end" />
+            <animate id="devscrollanim10" attributeName="x" from="0" to="50" dur="19s" begin="devscrollanim9.end" />
          </text>
          <text x="0" y="330" text-anchor="end" class={`${props.background} ${props.hide}`}>EBDEVELOPERWEBDEV
-            <animate id="scrollanim19" attributeName="x" from="100" to="0" dur="15s" begin="0s; scrollanim20.end" />
-            <animate id="scrollanim20" attributeName="x" from="0" to="100" dur="15s" begin="0s; scrollanim19.end" />
+            <animate id="devscrollanim19" attributeName="x" from="100" to="0" dur="15s" begin="0s; devscrollanim20.end" />
+            <animate id="devscrollanim20" attributeName="x" from="0" to="100" dur="15s" begin="devscrollanim19.end" />
          </text>
          <text x="0" y="400" text-anchor="end" class={`${props.background} ${props.hide}`}>ERWEBDEVELOPERWEB</text>
          <text x="0" y="400" fill="white" text-anchor="end" class={`${props.title} ${props.focus}`}>WEB</text>
          <text x="0" y="470" text-anchor="end" class={`${props.background} ${props.hide}`}>LOPERWEBDEVELOPER</text>
          <text x="0" y="470" fill="white" text-anchor="end" class={`${props.title} ${props.focus}`}>DEVELOPER</text>
          <text x="0" y="540" text-anchor="end" class={`${props.background} ${props.hide}`}>WEBDEVELOPERWEBDE
-            <animate id="scrollanim1" attributeName="x" from="50" to="0" dur="7s" begin="0s; scrollanim2.end" />
-            <animate id="scrollanim2" attributeName="x" from="0" to="50" dur="14s" begin="scrollanim1.end" />
+            <animate id="devscrollanim1" attributeName="x" from="50" to="0" dur="7s" begin="0s; devscrollanim2.end" />
+            <animate id="devscrollanim2" attributeName="x" from="0" to="50" dur="14s" begin="devscrollanim1.end" />
          </text>
          <text x="0" y="610" text-anchor="end" class={`${props.background} ${props.hide}`}>RWEBDEVELOPERWEBD
-            <animate id="scrollanim7" attributeName="x" from="25" to="50" dur="11s" begin="0s; scrollanim8.end" />
-            <animate id="scrollanim8" attributeName="x" from="50" to="25" dur="18s" begin="scrollanim7.end" />
+            <animate id="devscrollanim7" attributeName="x" from="25" to="50" dur="11s" begin="0s; devscrollanim8.end" />
+            <animate id="devscrollanim8" attributeName="x" from="50" to="25" dur="18s" begin="devscrollanim7.end" />
          </text>
          <text x="0" y="680" text-anchor="end" class={`${props.background} ${props.hide}`}>BDEVELOPERWEBDEVE
-            <animate id="scrollanim11" attributeName="x" from="0" to="50" dur="7s" begin="0;scrollanim12.end" fill="freeze" />
-            <animate id="scrollanim12" attributeName="x" from="50" to="0" dur="12s" begin="scrollanim11.end" fill="freeze" />
+            <animate id="devscrollanim11" attributeName="x" from="0" to="50" dur="7s" begin="0;devscrollanim12.end" />
+            <animate id="devscrollanim12" attributeName="x" from="50" to="0" dur="12s" begin="devscrollanim11.end" />
          </text>
          <text x="0" y="750" text-anchor="end" class={`${props.background} ${props.hide}`}>PERWEBDEVELOPERWE
-            <animate id="scrollanim3" attributeName="x" from="25" to="50" dur="10s" begin="0s; scrollanim4.end" />
-            <animate id="scrollanim4" attributeName="x" from="50" to="25" dur="12s" begin="scrollanim3.end" />
+            <animate id="devscrollanim3" attributeName="x" from="25" to="50" dur="10s" begin="0s; devscrollanim4.end" />
+            <animate id="devscrollanim4" attributeName="x" from="50" to="25" dur="12s" begin="devscrollanim3.end" />
          </text>
          <text x="0" y="820" text-anchor="end" class={`${props.background} ${props.hide}`}>WEBDEVELOPERWEBDE
-         <animate id="scrollanim5" attributeName="x" from="25" to="50" dur="8s" begin="0s; scrollanim6.end" />
-            <animate id="scrollanim6" attributeName="x" from="50" to="25" dur="15s" begin="scrollanim5.end" />
+            <animate id="devscrollanim5" attributeName="x" from="25" to="50" dur="8s" begin="0s; devscrollanim6.end" />
+            <animate id="devscrollanim6" attributeName="x" from="50" to="25" dur="15s" begin="devscrollanim5.end" />
+         </text>
+         <text x="0" y="890" text-anchor="end" class={`${props.background} ${props.hide}`}>WEBDEVELOPERWEBDE
+            <animate id="devscrollanim21" attributeName="x" from="0" to="100" dur="7s" begin="0s; devscrollanim22.end" />
+            <animate id="devscrollanim22" attributeName="x" from="100" to="0" dur="10s" begin="devscrollanim21.end" />
+         </text>
+         <text x="0" y="960" text-anchor="end" class={`${props.background} ${props.hide}`}>EVELOPERWEBDEVELO
+            <animate id="devscrollanim23" attributeName="x" from="75" to="0" dur="9s" begin="0s; devscrollanim24.end" />
+            <animate id="devscrollanim24" attributeName="x" from="0" to="75" dur="18s" begin="devscrollanim23.end" />
+         </text>
+         <text x="0" y="1030" text-anchor="end" class={`${props.background} ${props.hide}`}>PERWEBDEVELOPERWE
+            <animate id="devscrollanim25" attributeName="x" from="25" to="50" dur="7s" begin="0s; devscrollanim26.end" />
+            <animate id="devscrollanim26" attributeName="x" from="50" to="25" dur="15s" begin="devscrollanim25.end" />
+         </text>
+         <text x="0" y="1100" text-anchor="end" class={`${props.background} ${props.hide}`}>LOPERWEBDEVELOPER
+            <animate id="devscrollanim27" attributeName="x" from="50" to="0" dur="12s" begin="0s; devscrollanim28.end" />
+            <animate id="devscrollanim28" attributeName="x" from="0" to="50" dur="19s" begin="devscrollanim27.end" />
+         </text>
+         <text x="0" y="1170" text-anchor="end" class={`${props.background} ${props.hide}`}>EBDEVELOPERWEBDEV
+            <animate id="devscrollanim29" attributeName="x" from="100" to="0" dur="15s" begin="0s; devscrollanim30.end" />
+            <animate id="devscrollanim30" attributeName="x" from="0" to="100" dur="15s" begin="devscrollanim29.end" />
          </text>
       </svg>
    )
