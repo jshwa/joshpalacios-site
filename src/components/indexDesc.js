8c9594c8146ed1e7eae3b35f0f768fdd7b9708a1
jshwa <32717724+jshwa@users.noreply.github.com>
2018-04-25 18:01:18 -0500

    added short description for either section of the site to display on the index page

---
+++ src/components/indexDesc.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const DebateDesc = props => {
+   return (
+     <div id={props.id}>
+         Snackwave lomo bespoke quinoa helvetica. Coloring book taiyaki mustache occupy taxidermy affogato raw denim food truck put a bird on it intelligentsia 
+     </div>
+   )
+}
+
+export const DevDesc = props => {
+   return (
+     <div id={props.id}>
+         Snackwave lomo bespoke quinoa helvetica. Coloring book taiyaki mustache occupy taxidermy affogato raw denim food truck put a bird on it intelligentsia 
+     </div>
+   )
+}
