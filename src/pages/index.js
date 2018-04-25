844f73077de218e3863d1f9bd5d22e8a03647573
jshwa <32717724+jshwa@users.noreply.github.com>
2018-04-25 18:06:27 -0500

    remove excess from previous layout and simplified to fewer state variables and fewer animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,101 +5,50 @@ import styles from '../css/index.module.css';
 import '../css/index.css';
 import { DebateNav, DevNav } from '../components/circleNav';
 import { DebateBackground, DevBackground } from '../components/indexBackground';
+import { DebateDesc, DevDesc } from '../components/indexDesc';
 
 class IndexPage extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      main: null,
-      showNav: '',
-      removeDebate: '',
-      removeDev: '',
-      hidebackground: '',
-      focustitle: '',
-      slideImg: '',
-      svgPAR: '',
-      moveUp: '',
-      debateBgndColor: '',
-      devBgndColor: '',
+      active: '',
     };
   }
 
-  componentWillMount() {
-     const par = window.innerWidth < 1100 ? 'meet' : 'slice';
-     this.setState({
-        svgPAR: par
-     })
-  }
-
   mouseEnterDebate = () => {
-   this.state.main != 'debate' &&
+   this.state.active != 'Debater' &&
       this.setState({
-         main: 'debate',
-         showNav: <DebateNav id={styles.debatenav}/>,
-         hidebackground: styles.hidebackground,
-         focustitle: styles.focustitle,
-         slideImg: styles.imgSlideRight,
-         moveUp: {zIndex: '1', transitionDelay: "z-index 0s"},
-         debateBgndColor: {backgroundPosition: '25% 0%', transition: "background-position 3s ease-in-out"},
-      });
-  }
-
-  mouseEnterMain = () => {
-   this.state.main != null &&
-      this.setState({
-         main: null,
-         showNav: null,
-         removeDebate: '',
-         removeDev: '',
-         hidebackground: '',
-         focustitle: '',
-         slideImg:'',
-         moveUp: {zIndex: '1', transitionDelay: "z-index 1s"},
-         debateBgndColor: '',
-         devBgndColor: {transition: 'background-position 1s ease-in-out, z-index 2s'}
+         active: 'Debater',
       });
   }
 
    mouseEnterDev = () => {
-    this.state.main != 'dev' &&
+    this.state.active != 'Developer' &&
       this.setState({
-         main: 'dev',
-         showNav: <DevNav id={styles.devnav} />,
-         hidebackground: styles.hidebackground,
-         focustitle: styles.focustitle,
-         slideImg: styles.imgSlideLeft,
-         moveUp: {zIndex: '3'},
-         devBgndColor: {backgroundPosition: '50% 0%', transition: "background-position 3s ease-in-out"},
+         active: 'Developer',
       });
    } 
 
   render() {
-     const { showNav, removeDebate, removeDev, hidebackground, focustitle, slideImg, svgPAR, moveUp, debateBgndColor, devBgndColor } = this.state;
+     const { active } = this.state;
 
      return (
       <div class={styles.wrapper}>
-        {this.state.showNav}
-        <div id={styles.debate} style={debateBgndColor} onMouseEnter={this.mouseEnterDebate} >
-         <DebateBackground 
-            background={styles.backgroundsvg} 
-            hide={hidebackground} 
-            title={styles.titlesvg}
-            focus={focustitle}
-            PARval={svgPAR}/>
+        { this.state.active === "Debater" && <DebateNav id={styles.debatenav} /> }
+        { this.state.active === 'Developer' && <DevNav id={styles.devnav} /> }
+        <div id={styles.debate} onMouseEnter={this.mouseEnterDebate} />
+        <div id={styles.dev} onMouseEnter={this.mouseEnterDev} />
+        <div id={styles.title}>
+         <svg viewBox="0 0 80 20"  preserveAspectRatio="xMinYMin meet">
+            <text x="0" y="15">
+            {this.state.active}
+            </text>
+         </svg>
         </div>
-        <div id={styles.dev} style={{...moveUp, ...devBgndColor}} onMouseEnter={this.mouseEnterDev}>
-         <DevBackground 
-            background={styles.backgroundsvg} 
-            hide={hidebackground} 
-            title={styles.titlesvg}
-            focus={focustitle}
-            PARval={svgPAR}/>
-        </div>
-        <div  
-          id={styles.img} 
-          class={slideImg} 
-          onMouseEnter={this.mouseEnterMain}>
-          <Img sizes={this.props.data.josh.sizes} />
+        { this.state.active === "Debater" && <DebateDesc id={styles.debateDesc} /> }
+        { this.state.active === "Developer" && <DevDesc id={styles.devDesc} /> }
+        <div id={styles.img}>
+         { this.state.active === 'Developer' ? <Img sizes={this.props.data.joshFlipped.sizes} /> : <Img sizes={this.props.data.josh.sizes} /> }
         </div>
       </div>
     )
@@ -115,5 +64,10 @@ export const query = graphql`
        ...GatsbyImageSharpSizes_noBase64
       }
     }
-   }
+    joshFlipped: imageSharp(id: {regex: "/JoshLogoCircleFlipped.png/"}) {
+       sizes(maxWidth: 1080) {
+          ...GatsbyImageSharpSizes_noBase64
+       }
+    }
+  }
 `;
