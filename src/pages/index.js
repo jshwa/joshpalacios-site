659c997cd9173746fe6f31a009435aee68c5af67
jshwa <32717724+jshwa@users.noreply.github.com>
2018-05-05 13:29:52 -0500

    add onWheel event to direct to correct stack if the user scrolls, fixed props for React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Link from 'gatsby-link';
+import Link, { navigateTo } from 'gatsby-link';
 import Img from 'gatsby-image';
 import styles from '../css/index.module.css';
 import '../css/index.css';
 import { DebateNav, DevNav } from '../components/circleNav';
 import { DebateBackground, DevBackground } from '../components/indexBackground';
 import { DebateDesc, DevDesc } from '../components/indexDesc';
+import Debate from './debate';
+import Dev from './dev';
 
 class IndexPage extends React.Component {
   constructor(props){
@@ -29,29 +31,35 @@ class IndexPage extends React.Component {
       });
    } 
 
+   handleScroll = e => {
+     if (e.deltaY >= 10) {
+        this.state.active === "Debater" ? navigateTo('/debate') : navigateTo('/dev')
+     }
+   }
+
   render() {
      const { active } = this.state;
 
      return (
-      <div className={styles.wrapper}>
-        { this.state.active === "Debater" && <DebateNav id={styles.debatenav} /> }
-        { this.state.active === 'Developer' && <DevNav id={styles.devnav} /> }
-        <div id={styles.debate} onMouseEnter={this.mouseEnterDebate} />
-        <div id={styles.dev} onMouseEnter={this.mouseEnterDev} />
-        <div id={styles.title}>
-         <svg viewBox="0 0 80 20"  preserveAspectRatio="xMinYMin meet">
-            <text x="80" y="15" text-anchor="end">
-            {this.state.active}
-            </text>
-         </svg>
-        </div>
-        { this.state.active === "Debater" && <DebateDesc id={styles.debateDesc} /> }
-        { this.state.active === "Developer" && <DevDesc id={styles.devDesc} /> }
-        <div id={styles.img}>
-         { this.state.active === 'Developer' ? <Img sizes={this.props.data.joshFlipped.sizes} /> : <Img sizes={this.props.data.josh.sizes} /> }
-        </div>
-      </div>
-    )
+         <div className={styles.wrapper} onWheel={this.handleWheel}>
+            { this.state.active === "Debater" && <DebateNav id={styles.debatenav} /> }
+            { this.state.active === 'Developer' && <DevNav id={styles.devnav} /> }
+            <div id={styles.debate} onMouseEnter={this.mouseEnterDebate} />
+            <div id={styles.dev} onMouseEnter={this.mouseEnterDev} />
+            <div id={styles.title}>
+               <svg viewBox="0 0 80 20"  preserveAspectRatio="xMinYMin meet">
+                  <text x="80" y="15" textAnchor="end">
+                  {this.state.active}
+                  </text>
+               </svg>
+            </div>
+            { this.state.active === "Debater" && <DebateDesc id={styles.debateDesc} /> }
+            { this.state.active === "Developer" && <DevDesc id={styles.devDesc} /> }
+            <div id={styles.img}>
+               { this.state.active === 'Developer' ? <Img sizes={this.props.data.joshFlipped.sizes} /> : <Img sizes={this.props.data.josh.sizes} /> }
+            </div>
+         </div>
+      )
   }
 }
 
