18caa7333d02a71d38eea7f379c89178cc704acb
jshwa <32717724+jshwa@users.noreply.github.com>
2018-05-13 09:31:42 -0500

    add manifest and offline plugins, add favicon for manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,21 @@ module.exports = {
          path: `${__dirname}/src/img/`
       }
      },
+     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+         name: "JoshPalacios",
+         short_name: "Josh",
+         start_url: "/",
+         background_color: "#FFF",
+         theme_color: "#121212",
+         display: "stand-alone",
+         icon: "src/img/favicon.png",
+      }
+    },
      'gatsby-plugin-react-helmet',
      'gatsby-transformer-sharp',
      'gatsby-plugin-sharp',
+     'gatsby-plugin-offline',
    ],
 };
