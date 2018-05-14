f03b231abee27e47477c21b8f86e21a22a241a8b
jshwa <32717724+jshwa@users.noreply.github.com>
2018-05-14 00:40:35 -0500

    render static html to avoid errors

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { navigateTo } from 'gatsby-link';
 
 const NotFoundPage = () => (
-   navigateTo('/')
+  <div>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
 )
 
 export default NotFoundPage
